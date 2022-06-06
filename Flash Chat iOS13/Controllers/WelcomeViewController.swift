5e23402d4503da1d46b993d6be6f84f43a3448c1
Donna <donnajun1214@gmail.com>
2022-06-06 16:35:38 +0900

    use cltypinglabel

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,22 +7,24 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     var charIdx = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = ""
-       let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIdx, repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIdx +=  1
-        }
+//        titleLabel.text = ""
+//       let titleText = "⚡️FlashChat"
+//        for letter in titleText {
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIdx, repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIdx +=  1
+//        }
+        titleLabel.text = "⚡️FlashChat"
     }
     
 
